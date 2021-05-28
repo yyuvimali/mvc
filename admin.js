a5c09860856b8cc850b9ceb1b4436e29f9057b24
yyuvimali <82664190+yyuvimali@users.noreply.github.com>
2021-05-28 10:33:38 +0530

    mvc

---
+++ admin.js
@@ -0,0 +1,15 @@
+const path=require('path');
+
+const express=require('express');
+
+const productsController=require('../controller/products');
+
+const router=express.Router();
+
+router.get('/add-product',productsController.getAddProduct);
+
+
+router.post('/add-product',productsController.postAddProduct);
+
+module.exports=router;
+
---
+++ shop.js
@@ -0,0 +1,12 @@
+const path=require('path');
+
+const express=require('express');
+
+const productsController=require('../controller/products');
+
+const router=express.Router();
+
+router.get('/',productsController.getProducts)
+    
+    
+module.exports=router;
